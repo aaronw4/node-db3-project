30b7a6b34ae76699931050f3a71f3dd775781834
Aaron Williams <aaronrw4@yahoo.com>
2020-02-04 19:17:41 -0800

    schemes-model findById works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,13 @@ const db = require('../data/db.config');
 
 function find() {
     return db('schemes')
-}
+};
+
+function findById(id) {
+    return db('schemes').where({id})
+};
 
 module.exports = {
-    find
+    find,
+    findById
 }
